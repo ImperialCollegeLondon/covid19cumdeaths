5bdb47afcd2fa890ac76880333c9b41ecba0ea45
lucyokell <l.okell@imperial.ac.uk>
2020-06-13 13:03:00 +0100

    update processData restore country names

--- process_data.R
+++ process_data.R
@@ -90,14 +90,11 @@ saveRDS(df_data, "Output/deaths_processed.rds")
 # SEROLOGY
 
 # read in raw data
-sero_raw <- read.csv("Data/summary_sero_vs_deaths.csv", stringsAsFactors = FALSE)
-
-# remove Iran
-sero <- subset(sero_raw, country != "iran")
+sero <- read.csv("Data/summary_sero_vs_deaths.csv", stringsAsFactors = FALSE)
 
 # format country names as factors
 sero$country <- tools::toTitleCase(sero$country)
-sero_name <- c("Spain", "Sweden", "Switzerland", "Denmark")
+sero_name <- c("Spain", "Sweden", "Switzerland", "Denmark","Netherlands","United Kingdom")
 sero$country <- factor(sero$country, levels = sero_name)
 
 # get deaths per million population
